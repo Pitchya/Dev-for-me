901bca6086ce985d12aaf70c0b0a9cab211f329f
Pitchya <en_support02@wjtechnology.com>
2023-03-31 14:29:41 +0700

    Create Program.cs
    new project

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿
+    // See https://aka.ms/new-console-template for more information
+    /*Console.WriteLine("What's your name ??");
+    string name = Console.ReadLine();
+    Console.WriteLine("Hello " + name + " and surname" );
+    string surname = Console.ReadLine();
+    Console.WriteLine("How old's " + name);
+    Console.ReadLine();*/
+    //double time = DateTime;
+    //Console.WriteLine(time );
+
+    /*for (i=1; i<=4; i++){  //while (i<=4){
+        Console.WriteLine("A"+i);
+        //i = i + 1;
+    }*/
+    int i = 1;
+do
+{
+    Console.WriteLine("Hello" + i);
+    i = i + 1;
+}
+while (i <= 3);
+
+string[] name = { "max", "get", "jump", "yui" };
+foreach (string data in name)                   // loop.for.array(datatype newcon form array)
+{
+    Console.WriteLine(data);
+}
+Console.WriteLine("Thank you");
+
